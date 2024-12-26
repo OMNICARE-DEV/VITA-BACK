b3b9cda0ac1e50bb8534dd8a604a8b5ea91a9a1c
munjiyeon <moonja89@gmail.com>
2024-12-26 16:36:56 +0900

    swagger 적용

--- src/main/java/com/hops/hops_new_api/common/controller/UserLoginController.java
+++ src/main/java/com/hops/hops_new_api/common/controller/UserLoginController.java
@@ -6,6 +6,7 @@ import com.hops.hops_new_api.common.model.Constant;
 import com.hops.hops_new_api.common.model.HopsResponse;
 import com.hops.hops_new_api.common.model.Request.UserLoginRequest;
 import com.hops.hops_new_api.common.service.UserLoginService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "AUTH", description = "AUTH API")
+@Tag(name = "AUTH", description = "로그인 등 인증관련 API")
 @RestController
 @RequestMapping(value = "/user")
 @Slf4j
@@ -29,6 +30,7 @@ public class UserLoginController {
     }
 
     // 통합로그인 아이디 로그인
+    @Operation(summary = "로그인", description = "로그인 정보 조회")
     @PostMapping("/commonIdLogin")
     public HopsResponse commonIdLogin(@RequestBody UserLoginRequest request) throws HopsException {
         logger.info("UserLoginController.commonIdLogin request: {}", request);
---
+++ src/main/java/com/hops/hops_new_api/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.hops.hops_new_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("VITA API")
+                        .version("1.0")
+                        .description("VITA API 명세입니다."));
+    }
+}
