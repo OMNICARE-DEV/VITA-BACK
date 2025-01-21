3444c21af17d898cba70a156eb9853227cffd85e
JUNHO\ispin <ispinsa@gmail.com>
2025-01-21 09:28:49 +0900

    response body 로깅

--- src/main/java/com/vita/back/api/controller/ReservController.java
+++ src/main/java/com/vita/back/api/controller/ReservController.java
@@ -1,12 +1,13 @@
 package com.vita.back.api.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vita.back.api.model.Constant;
 import com.vita.back.api.model.VitaResponse;
+import com.vita.back.api.model.data.CommonUserDto;
 import com.vita.back.common.exception.VitaException;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,8 +18,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping(value = "/reserv")
 public class ReservController {
     @Operation(summary = "병원 조회", description = "병원 조회")
-    @PostMapping("/partnerCenterList")
+    @GetMapping("/partnerCenterList")
     public VitaResponse<?> partnerCenterList(@RequestParam(value="rosterNo") String request) throws VitaException {
-        return new VitaResponse<> (Constant.SUCCESS, false);
+        CommonUserDto ccc = new CommonUserDto();
+        ccc.setAddress("hjhjhjhjh");
+        
+        return new VitaResponse<> (Constant.SUCCESS, ccc);
     }
 }
