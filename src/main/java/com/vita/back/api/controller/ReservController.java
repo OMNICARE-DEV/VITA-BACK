d173fcc09c31de6cbd495db170f2be4319e43524
kimtaru <kimtaru2@gmail.com>
2025-01-31 13:19:41 +0900

    파일 다시 생

---
+++ src/main/java/com/vita/back/api/controller/ReservController.java
@@ -0,0 +1,28 @@
+package com.vita.back.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vita.back.api.model.Constant;
+import com.vita.back.api.model.VitaResponse;
+import com.vita.back.api.model.data.CommonUserDto;
+import com.vita.back.common.exception.VitaException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "예약관련 API", description = "")
+@RestController
+@RequestMapping(value = "/reserv")
+public class ReservController {
+    @Operation(summary = "병원 조회", description = "병원 조회")
+    @GetMapping("/partner-center-list")
+    public VitaResponse<?> selectPartnerCenterList(@RequestParam(value="rosterNo") String request) throws VitaException {
+        CommonUserDto ccc = new CommonUserDto();
+        ccc.setAddress("hjhjhjhjh");
+        
+        return new VitaResponse<> (Constant.SUCCESS, ccc);
+    }
+}
